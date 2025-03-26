68f0837dc889aa557666216223e961015079b324
Дима Фокша <timon.fok@outlook.com>
2025-03-26 12:45:03 +0200

    ch

--- Services/BuddyService.cs
+++ Services/BuddyService.cs
@@ -1,10 +1,5 @@
 ﻿using pj;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VoiceX.Enums;
 
 namespace VoiceX.Services
